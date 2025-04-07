d649bb3605388f8a8d772e012d9e48b8d6d5a159
Joshua.Whateley <Joshua.Whateley@medenterprises.com>
2025-04-07 21:01:00 +1200

    (bugfix) -> updates loading.tsx for every song [id] page as we want to keep this page in the future

--- src/app/played-songs/[song]/loading.tsx
+++ src/app/played-songs/[song]/loading.tsx
@@ -1,5 +1,15 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
-  return <Skeleton className="h-526px w-full" />;
+  return (
+    <div className="p-3 m-4 md:m-0 border-2 rounded-lg">
+      <div className="flex flex-col md:flex-row gap-8">
+        <Skeleton className="size-[356px] md:size-[500px] rounded-lg" />
+        <div className="flex flex-col justify-between p-3 gap-4 md:gap-0">
+          <Skeleton className="h-32 w-72 rounded-md" />
+          <Skeleton className="h-12 w-72 rounded-md" />
+        </div>
+      </div>
+    </div>
+  );
 }
